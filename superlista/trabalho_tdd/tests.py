554a011fc0d8647c8297b0f35b4fbe0bf9bc3b88
Jefferson S Rodrigues <jefferson2jsr@ufmg.br>
2021-05-13 17:43:23 -0300

    Adição do item Comprar anzol

--- superlista/trabalho_tdd/tests.py
+++ superlista/trabalho_tdd/tests.py
@@ -1,7 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
@@ -38,28 +37,36 @@ class NewVsitorTest(LiveServerTestCase):
         # aceita definir prioridades nas tarefas do tipo baixa, média e alta
         # Ela decide verificar a homepage
         baixa = self.browser.find_element_by_xpath('//label[@for="baixa"]').text
-        self.assertIn('Baixa', baixa)
+        self.assertIn('Low', baixa)
         media = self.browser.find_element_by_xpath('//label[@for="media"]').text
-        self.assertIn('Média', media)
+        self.assertIn('Average', media)
         alta = self.browser.find_element_by_xpath('//label[@for="alta"]').text
-        self.assertIn('Alta', alta)
+        self.assertIn('High', alta)
 
         # Ela percebe que o título da página e o cabeçalho mencionam
         # listas de tarefas com prioridade (priority to-do)
 
-        self.assertIn('priority to-do', self.browser.title)
+        self.assertIn('Priority To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('priority to-do', header_text)
+        self.assertIn('Priority To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa e a prioridade da
         # mesma imediatamente
-
         # Ela digita "Comprar anzol" em uma nova caixa de texto
         # e assinala prioridade alta no campo de seleção de prioridades
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        inputbox.send_keys('Comprar anzol')
+
+        ckalta = self.browser.find_element_by_id('alta')
+        ckalta.click()
 
         # Quando ela tecla enter, a página é atualizada, e agora
         # a página lista "1 - Comprar anzol - prioridade alta"
         # como um item em uma lista de tarefas
+
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1 Comprar anzol High')
 
         # Ainda continua havendo uma caixa de texto convidando-a a
         # acrescentar outro item. Ela insere "Comprar cola instantâne"
